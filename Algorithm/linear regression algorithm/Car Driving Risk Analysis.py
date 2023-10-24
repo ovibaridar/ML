9946a6ef9b3e7436f4b5bb8c81d861c8d419109f
ovibaridar <ovibaridar06@gmail.com>
2023-10-25 00:28:42 +0600

    LinearRegression

---
+++ Algorithm/linear regression algorithm/Car Driving Risk Analysis.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+# Load the dataset
+path = 'data sets/car driving risk analysis2.csv'
+datas = pd.read_csv(path)
+
+# Define features (X) and target (y)
+x = datas[['speed']]
+y = datas['risk']
+
+# Split the dataset into training and testing sets
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=.40, random_state=1)
+
+# Create and train the linear regression model
+rg = LinearRegression()
+rg.fit(xtrain, ytrain)
+
+# Make predictions on a new data point (e.g., speed = 90)
+prediction = rg.predict([[90]])
+print("Prediction:", prediction[0])
+
+# Calculate and print R-squared (R^2)
+y_pred = rg.predict(xtest)
+r2 = r2_score(ytest, y_pred)
+print("R-squared:", r2)
+print("M = ", rg.coef_)
+print("C = ", rg.intercept_)
